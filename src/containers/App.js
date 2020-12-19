3a3b2a3d832936e33ef4876230f7fcc0a2d7fc17
Kailin <kailin.tan.2010@gmail.com>
2020-12-19 11:53:42 +0800

    80 Building a react app 6

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-import CardList from './CardList';
-import SearchBox from './SearchBox';
-import Scroll from './Scroll';
+import CardList from '../components/CardList';
+import SearchBox from '../components/SearchBox';
+import Scroll from '../components/Scroll';
 import './App.css';
 class App extends Component {
   constructor() {
@@ -23,8 +23,8 @@ class App extends Component {
   }
 
   render() {
-    const filteredRobots = this.state.robots.filter(robots => {
-      return robots.name.toLowerCase().includes(this.state.searchField.toLowerCase());
+    const filteredRobots = this.state.robots.filter(robot => {
+      return robot.name.toLowerCase().includes(this.state.searchField.toLowerCase());
     });
     if (this.state.robots.length === 0) {
       return <h1> Loading </h1>
